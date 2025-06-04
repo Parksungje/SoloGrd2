cea4a070b936afbd72c09c53adbf6184a1a7ea29
Parksungje <Parksungje0213@gmail.com>
2025-06-04 23:12:11 +0900

    fix : Player Animation

--- Assets/Code/Players/CharacterMovement.cs
+++ Assets/Code/Players/CharacterMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21b55c720c052454c0e1bc9fb365c11ea0447a878a7afec6cca709e025256a07
-size 2833
+oid sha256:357ceb4b4da1d31cbd29236abc5c3de5c5cdd51c318b958b1fa755d9c552fca4
+size 2845
